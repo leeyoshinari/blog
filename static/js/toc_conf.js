cda0ebdd85fca6cc4bbf063fa00653dcb5d93dcc
leeyoshinari <lee_jc@outlook.com>
2024-07-23 09:00:19 +0800

    update

--- static/js/toc_conf.js
+++ static/js/toc_conf.js
@@ -6,14 +6,15 @@ var jquery_ztree_toc_opts = {
         width:'23%',
         'margin-left': '1%',
         'margin-top': '2%',
+        'padding-bottom': '3%',
         overflow: 'auto',
         position: 'fixed',
         'z-index': 2147483647,
         border: '0px none',
         left: '0px',
         top: '0px',
-				// 'overflow-x': 'hidden',
-				'height': $(window).height() + 'px'
+        // 'overflow-x': 'hidden',
+        'height': $(window).height() + 'px'
     }
 }
 var markdown_panel_style = {
